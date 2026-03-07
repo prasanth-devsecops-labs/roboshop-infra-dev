dd141265d72d0bc1a7c4e76af2e3845840557615
Prasanth Reddy <prasanthreddy1792@gmail.com>
2026-03-07 21:24:17 +0530

    terraform robo infra

---
+++ 30-bastioon/bastion.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# We are creating 50GB root disk, only 20GB is partitioned
+# Remaining 30GB need to extend using belwo commands
+growpart /dev/nvme0n1p4 4
+lvextend -r -L +30G /dev/mapper/RootVG-homeVol
+xfs_growfs /home
+
+yum install -y yum-utils
+yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
+yum -y install terraform
