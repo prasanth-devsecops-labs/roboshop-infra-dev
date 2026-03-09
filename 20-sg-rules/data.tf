a39b576a189ff97bc3a0c6ffe5724c1b2b1a7343
Prasanth Reddy <prasanthreddy1792@gmail.com>
2026-03-09 14:35:23 +0530

    terraform robo infra

--- 20-sg-rules/data.tf
+++ 20-sg-rules/data.tf
@@ -18,6 +18,10 @@ data "aws_ssm_parameter" "mysql_sg_id" {
   name = "/${var.project_name}/${var.env}/mysql_sg_id"
 }
 
+data "aws_ssm_parameter" "rabbitmq_sg_id" {
+  name = "/${var.project_name}/${var.env}/rabbitmq_sg_id"
+}
+
 data "aws_ssm_parameter" "catalogue_sg_id" {
   name = "/${var.project_name}/${var.env}/catalogue_sg_id"
 }
