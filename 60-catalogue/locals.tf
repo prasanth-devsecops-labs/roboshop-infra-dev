8615195e8b7089fdaa223b21d24d21304db7f387
Prasanth Reddy <prasanthreddy1792@gmail.com>
2026-03-13 13:29:06 +0530

    terraform robo infra

--- 60-catalogue/locals.tf
+++ 60-catalogue/locals.tf
@@ -8,4 +8,5 @@ locals {
   ami_id = data.aws_ami.devops.id
   catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
   private_subnet_id = split(",", data.aws_ssm_parameter.private_subnet_ids.value)[0]
+  vpc_id = data.aws_ssm_parameter.vpc_id.value
 }
--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -52,3 +52,84 @@ resource "aws_ami_from_instance" "catalogue" {
   depends_on = [ aws_ec2_instance_state.catalogue ]
 }
 
+resource "aws_lb_target_group" "catalogue" {
+  name     = "${var.project}-${var.environment}-catalogue"
+  port     = 8080
+  protocol = "HTTP"
+  vpc_id   = local.vpc_id
+  deregistration_delay = 60
+  health_check {
+    healthy_threshold = 2
+    interval = 10
+    matcher = "200-299"
+    path = "/health"
+    port = 8080
+    protocol = "HTTP"
+    timeout = 2
+    unhealthy_threshold = 3
+  }
+}
+
+resource "aws_launch_template" "catalogue" {
+  name = "${var.project}-${var.environment}-catalogue"
+
+  image_id = aws_ami_from_instance.catalogue.id
+  instance_initiated_shutdown_behavior = "terminate"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [local.catalogue_sg_id]
+  update_default_version = true
+
+    tag_specifications {
+        resource_type = "instance"
+        tags = merge(
+        {
+            Name = "${var.project}-${var.environment}-catalogue"
+        },
+        local.common_tags
+        )
+    }
+
+    tag_specifications {
+        resource_type = "volumes"
+        tags = merge(
+            {
+                Name = "${var.project}-${var.environment}-catalogue"
+            },
+            local.common_tags
+            )
+    }
+
+    tags = merge(
+        {
+            Name = "${var.project}-${var.environment}-catalogue"
+        },
+        local.common_tags
+    )
+}
+
+# resource "aws_autoscaling_group" "catalogue" {
+#   name                      = "${var.project}-${var.environment}-catalogue"
+#   max_size                  = 10
+#   min_size                  = 1
+#   health_check_grace_period = 120
+#   health_check_type         = "ELB"
+#   desired_capacity          = 1
+#   force_delete              = false
+#   launch_template {
+#     id = aws_launch_template.catalogue.id
+#     version = "$Latest"
+#   }
+
+#   vpc_zone_identifier       = [local.private_subnet_id]
+#   target_group_arns = [aws_lb_target_group.catalogue.arn]
+
+#   tag {
+#     key                 = "Name"
+#     value               = "${var.project}-${var.environment}-catalogue"
+#     propagate_at_launch = true
+#   }
+
+#   timeouts {
+#     delete = "15m"
+#   }
+# }
