63349e052a01f2bb74596940684514fbe986b8d3
Prasanth Reddy <prasanthreddy1792@gmail.com>
2026-03-13 12:11:39 +0530

    terraform robo infra

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -14,21 +14,41 @@ resource "aws_instance" "catalogue" {
 resource "terraform_data" "bootstrap_catalogue" {
   triggers_replace = [aws_instance.catalogue.id]
 
-connection {
-    type = "ssh"
-    user = "ec2-user"
-    password = "DevOps321"
-    host = aws_instance.catalogue.private_ip
+    connection {
+        type = "ssh"
+        user = "ec2-user"
+        password = "DevOps321"
+        host = aws_instance.catalogue.private_ip
+    }
+
+    provisioner "file" {
+    source = "bootstrap.sh"
+    destination = "/tmp/bootstrap.sh"
+    }
+    provisioner "remote-exec" {
+        inline = [
+        "chmod +x /tmp/bootstrap.sh",
+        "sudo sh /tmp/bootstrap.sh catalogue dev"
+        ]
+    }
+    # depends_on = [ aws_instance.catalogue ]
 }
 
-provisioner "file" {
-  source = "bootstrap.sh"
-  destination = "/tmp/bootstrap.sh"
+resource "aws_ec2_instance_state" "catalogue" {
+  instance_id = aws_instance.catalogue.id
+  state       = "stopped"
+  depends_on = [ terraform_data.bootstrap_catalogue ]
 }
-provisioner "remote-exec" {
-    inline = [
-      "chmod +x /tmp/bootstrap.sh",
-      "sudo sh /tmp/bootstrap.sh catalogue dev"
-     ]
-  }
+
+resource "aws_ami_from_instance" "catalogue" {
+  name               = "${var.project}-${var.environment}-catalogue"
+  source_instance_id = aws_instance.catalogue.id
+  tags = merge(
+    {
+        Name = "${var.project}-${var.environment}-catalogue"
+    },
+    local.common_tags
+  )
+  depends_on = [ aws_ec2_instance_state.catalogue ]
 }
+
