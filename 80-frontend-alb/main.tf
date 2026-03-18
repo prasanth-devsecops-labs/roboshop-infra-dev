cffe3098e905b4f9a12e03ce8c668fd345a41ea6
Prasanth Reddy <prasanthreddy1792@gmail.com>
2026-03-18 23:52:10 +0530

    terraform robo infra

--- 80-frontend-alb/main.tf
+++ 80-frontend-alb/main.tf
@@ -33,7 +33,7 @@ resource "aws_lb_listener" "http" {
 
 resource "aws_route53_record" "www" {
   zone_id = var.zone_id
-  name    = "*.frontend-alb-${var.environment}.${var.domain_name}"
+  name    = "*.${var.domain_name}"
   type    = "A"
   
   # load balancer details
