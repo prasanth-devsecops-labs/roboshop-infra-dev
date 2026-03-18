3934c7a43c48c5d87aa7e170e6305fee0c313b3d
Prasanth Reddy <prasanthreddy1792@gmail.com>
2026-03-18 23:40:24 +0530

    terraform robo infra

---
+++ 80-frontend-alb/main.tf
@@ -0,0 +1,47 @@
+resource "aws_lb" "frontend_alb" {
+  name               = "${var.project}-${var.environment}-frontend"
+  internal           = true
+  load_balancer_type = "application"
+  security_groups    = [local.frontend_alb_sg_id]
+  subnets            = local.public_subnet_ids
+
+  enable_deletion_protection = false
+
+  tags = merge(
+    local.common_tags,
+    {
+        Name = "${var.project}-${var.environment}"
+    })
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.frontend_alb.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  certificate_arn   = local.frontend_alb_certificate_arn
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/html"
+      message_body = "<h1>Hi, I am from frontend ALB</h1>"
+      status_code  = "200"
+    }
+  }
+}
+
+resource "aws_route53_record" "www" {
+  zone_id = var.zone_id
+  name    = "*.frontend-alb-${var.environment}.${var.domain_name}"
+  type    = "A"
+  
+  # load balancer details
+  alias {
+    name                   = aws_lb.frontend_alb.dns_name
+    zone_id                = aws_lb.frontend_alb.zone_id
+    evaluate_target_health = true
+  }
+  allow_overwrite = true
+
+}
