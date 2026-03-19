38c8d8b0ffca9258996ac476cb83881e34659d73
Prasanth Reddy <prasanthreddy1792@gmail.com>
2026-03-19 14:53:06 +0530

    terraform robo infra

--- 90-components/variables.tf
+++ 90-components/variables.tf
@@ -4,18 +4,18 @@ variable "components" {
     catalogue = {
         rule_priority = 10
     }
-    # user = {
-    #     rule_priority = 20
-    # }
-    # cart = {
-    #     rule_priority = 30
-    # }
-    # shipping = {
-    #     rule_priority = 40
-    # }
-    # payment = {
-    #     rule_priority = 50
-    # }
+    user = {
+        rule_priority = 20
+    }
+    cart = {
+        rule_priority = 30
+    }
+    shipping = {
+        rule_priority = 40
+    }
+    payment = {
+        rule_priority = 50
+    }
 
     # this attaching to frontend ALB, there is only component there
     frontend = {
