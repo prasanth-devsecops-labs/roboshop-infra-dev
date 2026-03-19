280fcd4cf0db2949bd84952515db5d6381b4c985
Prasanth Reddy <prasanthreddy1792@gmail.com>
2026-03-19 16:09:05 +0530

    terraform robo infra

---
+++ 95-cdn/variables.tf
@@ -0,0 +1,19 @@
+variable "project" {
+  default = "roboshop"
+  type    = string
+}
+
+variable "environment" {
+  default = "dev"
+  type    = string
+}
+
+variable "zone_id" {
+    type = string
+    default = "Z082901534H7OC4NT02WB"
+}
+
+variable "domain_name" {
+    type = string
+    default = "prashum.online"
+}
